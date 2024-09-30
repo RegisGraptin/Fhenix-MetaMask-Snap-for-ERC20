ba3d0c07224156de4e80b98154b9b7fb3a2f761d
rere <regis-graptin@sfr.fr>
2024-09-30 12:31:56 +0800

    feat: create basic home page

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,5 +1,5 @@
-import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { Box, Text, Bold } from '@metamask/snaps-sdk/jsx';
+import { type OnRpcRequestHandler, type OnHomePageHandler, type OnUserInputHandler, UserInputEventType } from '@metamask/snaps-sdk';
+import { Heading, Box, Text, Bold, Form, Input, Button } from '@metamask/snaps-sdk/jsx';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -41,3 +41,102 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
       throw new Error('Method not found.');
   }
 };
+
+type ERC20 = {
+  name: string;
+  address: string;
+};
+
+
+
+async function fetchAllRegisteredERC() {
+  
+  // Read storage dtaa
+  const persistedData = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  })
+
+  console.log("Extracted data", persistedData);
+
+  // const ercTokens: undefined | ERC20[] = persistedData["ercTokens"];
+
+  // console.log("Extracted token", ercTokens);
+
+  return (
+    
+      <Heading>ERC-20 encrypted tokens</Heading>
+    
+  );
+}
+
+  {/* {ercTokens && ercTokens.map((ercToken, i) => {
+        return (
+          <Text>Here some value</Text>
+        )
+      })} */}
+    
+export const onHomePage: OnHomePageHandler = async () => {
+
+  console.log("Here home page");
+
+  const persistedData = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  })
+  
+
+
+    {/* fetchAllRegisteredERC() */}
+
+  return {
+    content: (
+      <Box>
+        
+        <Heading>ERC-20 encrypted tokens</Heading>
+        <Text>Welcome to my Snap home page!</Text>
+        <Form name='input-form'>
+          <Input name='token-address' placeholder='ERC20 address' />
+          <Button type='submit'>Add</Button>
+        </Form>
+      </Box>
+    ),
+  };
+};
+
+
+export const onUserInput: OnUserInputHandler = async ({id, event}) => {
+  if (event.type === UserInputEventType.FormSubmitEvent) {
+
+
+    let fakeToken: ERC20 = {
+      address: "0x0000000000001",
+      name: "FTM"
+    }
+
+    await snap.request({
+      method: "snap_manageState",
+      params: {
+        operation: "update",
+        newState: { ercTokens: [fakeToken] },
+      },
+    })
+
+    
+    console.log("Added value:")
+    console.log(event.value["token-address"])
+
+    await snap.request({
+      method: "snap_updateInterface",
+      params: {
+        id, 
+        ui: (
+          <Box>
+            <Heading>Here is the form after execution</Heading>
+            <Text>{event.value["token-address"]}</Text>
+          </Box>
+        )
+      }
+    });
+  }
+}
